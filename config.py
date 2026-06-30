e3352b790a5502f7fb961453723fb32d5cb33110
Byre Gowda S K <byregowdabyregowda146@gmail.com>
2026-06-30 21:02:27 +0530

    config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Gagan@1606",
-        database="career_db"
+        password="1234567890",
+        database="1234567890"
     )
