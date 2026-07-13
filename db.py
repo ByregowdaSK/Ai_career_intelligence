edd387db997f7bbca1b5da59e1bc68ce56929ddd
Byre Gowda S K <byregowdabyregowda146@gmail.com>
2026-07-13 12:55:01 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1234567890",
-        database="1234567890"
+        password="...",
+        database="..."
     )
